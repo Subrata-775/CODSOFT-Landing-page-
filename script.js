7b20e7251e4086e90c6a754008877bbd558ac508
Subrata Jana <subratajana7735@gmail.com>
2024-10-30 20:47:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.order-btn').addEventListener('click', () => {
+    alert('Thank you for placing an order!');
+});
+
+document.querySelector('.hero-buttons button:nth-child(2)').addEventListener('click', () => {
+    alert('Order Now clicked!');
+});
